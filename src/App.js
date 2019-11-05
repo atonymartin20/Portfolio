d24382bfcbd07e7a2a47d2d9998168de8fb12c9c
Axis <atonymartin20@yahoo.com>
2019-11-04 20:06:15 -0600

    Beginning to create Routes and portfolio structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
-
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Navbar from './Components/Navbar';
+import Homepage from './Components/Homepage';
+import Projects from './Components/Projects';
+import ErrorPage from './Components/ErrorPage';
+
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route path="/projects">
+                    <Projects />
+                </Route>
+                <Route>
+                    <ErrorPage />
+                </Route>
+            </Switch>
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/StyledComponents/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const NavbarDiv = styled.div`
+
+`;
+
+export {
+    NavbarDiv,
+}
