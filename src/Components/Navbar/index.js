a5dadc2a9555df130ac30ff4d1fbe63bc92f460e
Axis <atonymartin20@yahoo.com>
2019-11-12 22:00:05 -0600

    Working on adding drawer for mobile view of Navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import classNames from 'classnames'
-import PropTypes from "prop-types";
+import classNames from "classnames";
 import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -11,32 +10,38 @@ import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
 import Menu from "@material-ui/icons/Menu";
 import Close from "@material-ui/icons/Close";
-import { blue } from '@material-ui/core/colors';
-import { withTheme } from '@material-ui/styles';
+import { blue } from "@material-ui/core/colors";
+import { withTheme } from "@material-ui/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const styles = () => ({
     appBar: {
         display: "flex",
-        color: 'white',
+        color: "white",
         width: "100%",
         boxShadow: "none",
         transition: "all 500ms ease 0s",
         alignItems: "center",
         flexFlow: "row nowrap",
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
         position: "fixed",
         height: 65,
         top: "0",
-        padding: '0px 4%',
-        fontSize: '2.5rem',
-    }, 
+        padding: "0px 4%",
+        fontSize: "2.5rem"
+    },
     backgroundBlue: {
-        backgroundColor: '#0E3386',
+        backgroundColor: "#0E3386"
     },
     backgroundTransparent: {
-        backgroundColor: 'transparent',
+        backgroundColor: "transparent"
+    },
+    logo: {
+        fontFamily: "Rye, cursive",
+        fontSize: "3.0rem"
     }
-
 });
 
 class Navbar extends React.Component {
@@ -45,52 +50,50 @@ class Navbar extends React.Component {
         this.state = {
             drawerOpen: false
         };
-        // this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
         this.NavbarChangeColor = this.NavbarChangeColor.bind(this);
     }
 
     componentDidMount() {
-        window.addEventListener("scroll", this.NavbarChangeColor)
+        window.addEventListener("scroll", this.NavbarChangeColor);
     }
 
     NavbarChangeColor() {
         const { classes } = this.props;
         const windowsScrollTop = window.pageYOffset;
-        if(windowsScrollTop > 300) {
+        if (windowsScrollTop > 300) {
             document.body
-            .getElementsByTagName("header")[0]
-            .classList.remove(classes.backgroundTransparent);
-          document.body
-            .getElementsByTagName("header")[0]
-            .classList.add(classes.backgroundBlue);
+                .getElementsByTagName("header")[0]
+                .classList.remove(classes.backgroundTransparent);
+            document.body
+                .getElementsByTagName("header")[0]
+                .classList.add(classes.backgroundBlue);
         } else {
-          document.body
-            .getElementsByTagName("header")[0]
-            .classList.remove(classes.backgroundBlue);
-          document.body
-            .getElementsByTagName("header")[0]
-            .classList.add(classes.backgroundTransparent);
+            document.body
+                .getElementsByTagName("header")[0]
+                .classList.remove(classes.backgroundBlue);
+            document.body
+                .getElementsByTagName("header")[0]
+                .classList.add(classes.backgroundTransparent);
         }
     }
-    
-//     <Header
-//     brand="Material Kit PRO React"
-//     links={<HeaderLinks dropdownHoverColor="info" />}
-//     fixed
-//     color="transparent"
-//     changeColorOnScroll={{
-//       height: 400,
-//       color: "info"
-//     }}
-//   />
+
     render() {
         const { classes } = this.props;
 
         return (
-            <AppBar className={classes.appBar + ' ' + classes.backgroundTransparent}>
-                <h1>test</h1>
+            <AppBar className={classes.appBar}>
+                <Hidden smUp>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={this.handleDrawerToggle}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Hidden>
+                <h1 className={classes.logo}>Alex Martin</h1>
             </AppBar>
-        )
+        );
     }
 }
 
