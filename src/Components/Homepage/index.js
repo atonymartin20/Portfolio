d4e327d6687977766cff166466f12705eccc9aad
Axis <atonymartin20@yahoo.com>
2019-11-18 22:45:06 -0600

    Continued work on the portfolio

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import { HomepageDiv, HomepageTopImg } from '../StyledComponents';
+import { HomepageDiv, HomepageTopImg, HomepageHeader } from '../StyledComponents';
 
 class Homepage extends React.Component {
     render() {
@@ -8,7 +8,11 @@ class Homepage extends React.Component {
             <HomepageDiv>
                 <Navbar />
                 <HomepageTopImg>
-                    <h1>Layer 1</h1>
+                    <HomepageHeader>
+                        <h1>Alex Martin</h1>
+                        <h2>Front-end Developer</h2>
+
+                    </HomepageHeader>
                 </HomepageTopImg>
                 <h1>lolz</h1>
             </HomepageDiv>
--- src/Components/StyledComponents/index.js
+++ src/Components/StyledComponents/index.js
@@ -28,8 +28,25 @@ const HomepageTopImg = styled.div`
     height: 600px;
 `;
 
+const HomepageHeader = styled.header`
+    box-sizing: border-box;
+    text-align: center;
+    background-color: rgba(43, 43, 43, 0.1);
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 100px 2.5%;
+    text-shadow: 1px 1px 2px black, 1px 0px 2px black, 0px 1px 2px black, 0 0 8px #333;
+    height: 600px;
+    color: white;
+    font-size: 3.5rem;
+`;
+
 export {
     NavbarDiv,
     HomepageDiv,
     HomepageTopImg,
+    HomepageHeader,
 }
