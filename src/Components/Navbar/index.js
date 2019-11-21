ba83d7d03baaf2d27e76dbfa7ac4059a14b05ca0
Axis <atonymartin20@yahoo.com>
2019-11-20 22:48:29 -0600

    Green 3

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -324,7 +324,7 @@ class Navbar extends React.Component {
                             }}
                         >
                             {drawer}
-                            {/* Green 2 */}
+                            {/* Green 3 */}
                         </Drawer>
                     </Hidden>
                 </nav>
