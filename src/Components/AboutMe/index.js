4b0601f6c848d16db8aed34824666c09edc66c14
Axis <atonymartin20@yahoo.com>
2020-01-22 14:47:27 -0600

    Completed About Me Page

--- src/Components/AboutMe/index.js
+++ src/Components/AboutMe/index.js
@@ -11,7 +11,6 @@ const styles = theme => ({
     },
     aboutMeH1: {
         fontSize: '4.5rem',
-        // margin: '0 auto',
         marginBottom: 20,
         width: '100%',
         textAlign: 'center',
@@ -44,20 +43,64 @@ const styles = theme => ({
         width: '100%',
         lineHeight: '1.15',
         maxWidth: 1000,
-        [theme.breakpoints.down(800)]: {
-            fontSize: '2.5rem',
+        [theme.breakpoints.down(1475)]: {
+           maxWidth: 900,
         },
-        [theme.breakpoints.down(600)]: {
-            fontSize: '2.2rem',
+        [theme.breakpoints.down(1375)]: {
+            maxWidth: 800,
+         },
+         [theme.breakpoints.down(1160)]: {
+            maxWidth: 700,
+         },
+         [theme.breakpoints.down(1060)]: {
+            maxWidth: 600,
+         },
+        [theme.breakpoints.down(1000)]: {
+            fontSize: '2.25rem',
         },
-        [theme.breakpoints.down(500)]: {
+        [theme.breakpoints.down(700)]: {
             fontSize: '2.0rem',
+        },
+        [theme.breakpoints.down(550)]: {
+            fontSize: '1.85rem',
         },
         [theme.breakpoints.down(400)]: {
             fontSize: '1.7rem',
         },
     },
-
+    headshot: {
+        [theme.breakpoints.down(2000)]: {
+            width: 600,
+        },
+        [theme.breakpoints.down(1700)]: {
+            width: 500,
+        },
+        [theme.breakpoints.down(1650)]: {
+            width: 450,
+        },
+        [theme.breakpoints.down(1600)]: {
+            width: 400,
+        },
+        [theme.breakpoints.down(1300)]: {
+            width: 350,
+        },
+        [theme.breakpoints.down(1220)]: {
+            width: 300,
+        },
+        [theme.breakpoints.down(950)]: {
+            marginTop: 15,
+            width: 450,
+        },
+        [theme.breakpoints.down(550)]: {
+            width: 425,
+        },
+        [theme.breakpoints.down(400)]: {
+            width: 300,
+        },
+        [theme.breakpoints.down(300)]: {
+            width: '100%',
+        },
+    },
 })
 
 class AboutMe extends React.Component {
@@ -74,7 +117,7 @@ class AboutMe extends React.Component {
                         I've spent time working in a multitude of industries. I've worked in a factory helping to make sure that production quotas were consistently hit. I've also worked in an environment where I was communicating with customers on a daily basis, and worked to improve the user experience. I am interested in combining those skills in a team environment building cutting-edge projects.
                         <br /><br />
                         In my free time I enjoy cooking, watching auto racing, playing softball, and flying my drone simulation software. If you want to know more about me, please get in touch. </h1>
-                        <img src={Headshot} />
+                        <img src={Headshot} className={classes.headshot} alt='Alex Martin Headshot' />
 
                     </header>
             </div>
